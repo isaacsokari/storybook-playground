c67dbc27ff8559c0d8f4535817ee2a4ce99b362f
Isaac Sokari <isaacsokari@gmail.com>
2021-03-19 03:43:01 +0100

    changed to dark theme, and added custom theme file

---
+++ .storybook/manager.js
@@ -0,0 +1,8 @@
+import { addons } from '@storybook/addons';
+import { themes } from '@storybook/theming';
+// import myTheme from './myTheme';
+
+addons.setConfig({
+  theme: themes.dark,
+  // theme: myTheme,
+});
